7fdac67d81026c49434be1c2df91ca7032614c20
VM <97495781+TANADOSVV@users.noreply.github.com>
2023-06-01 16:46:54 -0300

    tentativa8.1

--- configuration.c
+++ configuration.c
@@ -1765,7 +1765,7 @@ static struct config_bool_setting *populate_settings_bool(
    SETTING_BOOL("audio_enable_menu_notice",      &settings->bools.audio_enable_menu_notice, true, DEFAULT_AUDIO_ENABLE_MENU_NOTICE, false);
    SETTING_BOOL("audio_enable_menu_bgm",         &settings->bools.audio_enable_menu_bgm, true, DEFAULT_AUDIO_ENABLE_MENU_BGM, false);
    SETTING_BOOL("audio_enable_menu_scroll",      &settings->bools.audio_enable_menu_scroll, true, DEFAULT_AUDIO_ENABLE_MENU_SCROLL, false);
-   SETTING_BOOL("musica_no_menu_option",         &settings->bools.musica_no_menu_option, true, musica_no_menu_option, false);
+   SETTING_BOOL("musica_no_menu_option",         &settings->bools.musica_no_menu_option, true, DEFAULT_MUSICA_NO_MENU_OPTION, false);
    SETTING_BOOL("audio_mute_enable",             audio_get_bool_ptr(AUDIO_ACTION_MUTE_ENABLE), true, false, false);
 #ifdef HAVE_AUDIOMIXER
    SETTING_BOOL("audio_mixer_mute_enable",       audio_get_bool_ptr(AUDIO_ACTION_MIXER_MUTE_ENABLE), true, false, false);
